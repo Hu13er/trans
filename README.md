faa72eebf61c72e564ecb165153aa8c9305b8029
faramin esmaili seraji <faramin.smile@gmail.com>
2017-08-10 02:25:31 +0430

    first try to impliment.

---
+++ README.md
@@ -0,0 +1,17 @@
+## Translate
+
+Use fastdic website to translate words to persion.
+
+
+### Install
+
+```bash
+$ go get github.com/Hu13er/trans
+```
+
+### Usage
+
+```bash
+$ trans <statement>
+```
+
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var (
+	extractRegexp = regexp.MustCompile(`<ul.*?class="result js-result".*?>(?P<result>.*?)<\/ul>`)
+	translateURL  = "https://fastdic.com/word/%s"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		help()
+		os.Exit(0)
+	}
+
+	stmt := os.Args[1]
+
+	html := getHTML(stmt)
+	if html == "" {
+		os.Exit(0)
+	}
+
+	trans := extract(html)
+	fmt.Println(trans)
+}
+
+func extract(html string) string {
+	var result string
+	result = regexp.MustCompile("\n").ReplaceAllString(html, "")
+	for k, v := range extractRegexp.SubexpNames() {
+		if v == "result" {
+			result = extractRegexp.FindStringSubmatch(result)[k]
+		}
+	}
+
+	result = regexp.MustCompile("<br>").ReplaceAllString(result, "\n")
+	result = regexp.MustCompile("<.*?>").ReplaceAllString(result, "")
+
+	return result
+}
+
+func getHTML(stmt string) string {
+	url := fmt.Sprintf(translateURL, stmt)
+	resp, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	return string(html)
+}
+
+func help() {
+	help := "Usage: trans <statement>"
+	fmt.Println(strings.TrimSpace(help))
+}
